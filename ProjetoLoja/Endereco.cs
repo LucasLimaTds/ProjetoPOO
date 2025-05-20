f1f18c8b9ac278187cfb7598ef932fb8454dfd5f
Lucas Lima Tedesco <lutedesco03@gmail.com>
2025-05-20 20:21:55 -0300

    Criada classe Endereço e classe Usuário

---
+++ ProjetoLoja/Endereco.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ProjetoLoja;
+
+public class Endereco
+{
+    public String Rua { get; set; }
+    public String Numero { get; set; }
+    public String Complemento { get; set; }
+    public String Bairro { get; set; }
+    public String CEP { get; set; }
+    public String Cidade { get; set; }
+    public String Estado { get; set; }
+}
--- ProjetoLoja/Usuario.cs
+++ ProjetoLoja/Usuario.cs
@@ -4,5 +4,7 @@ namespace ProjetoLoja;
 
 public class Usuario
 {
-    
+    public String Nome { get; set; }
+    public String Senha { get; set; }
+    int DireitosDeUsuario { get; set; }
 }
